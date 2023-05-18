cdc8cb04411f9468f0cbb9a699fce2b6051919db
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-18 15:47:06 +0900

    bin

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-echo "Build script executed."
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
